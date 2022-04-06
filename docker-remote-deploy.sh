bb548bd9a11fd2fdf166ecc4a595b2d0c7de5b3a
Jan Illetschko <jan.illetschko@3kraft.com>
2022-04-06 16:57:02 +0200

    adds inputs for docker host user and docker registry

--- docker-remote-deploy.sh
+++ docker-remote-deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE root@$DOCKER_HOST "docker login ghcr.io -u $REGISTRY_USER -p $REGISTRY_TOKEN"
-ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE root@$DOCKER_HOST "docker stop $CONTAINER_NAME"
-ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE root@$DOCKER_HOST "docker rm $CONTAINER_NAME"
-ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE root@$DOCKER_HOST "docker run -d --name $CONTAINER_NAME -p $CONTAINER_PORTS $CONTAINER_ARGS $IMAGE_NAME:$TAG_NAME"
-ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE root@$DOCKER_HOST "docker image prune -af"
+ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker login $REGISTRY -u $REGISTRY_USER -p $REGISTRY_TOKEN"
+ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker stop $CONTAINER_NAME"
+ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker rm $CONTAINER_NAME"
+ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker run -d --name $CONTAINER_NAME -p $CONTAINER_PORTS $CONTAINER_ARGS $IMAGE_NAME:$TAG_NAME"
+ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker image prune -af"
