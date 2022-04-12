39793c3184023344c5bdee9562dd652239812689
Jan Illetschko <jan.illetschko@3kraft.com>
2022-04-12 17:36:32 +0200

    pulls newest container image before runing

--- docker-remote-deploy.sh
+++ docker-remote-deploy.sh
@@ -4,5 +4,6 @@ set -e
 ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker login $REGISTRY -u $REGISTRY_USER -p $REGISTRY_TOKEN"
 ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker stop $CONTAINER_NAME"
 ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker rm $CONTAINER_NAME"
+ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker pull $IMAGE_NAME:$TAG_NAME"
 ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker run -d --name $CONTAINER_NAME -p $CONTAINER_PORTS $CONTAINER_ARGS $IMAGE_NAME:$TAG_NAME"
 ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY_FILE $DOCKER_HOST_USER@$DOCKER_HOST "docker image prune -af"
